9f107235e6c05c3d2f89cc0a5d2fb9e8406ece83
Miguel S. Ramos <80494880+miguel-sr@users.noreply.github.com>
2021-09-08 19:16:42 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 // Hamburguer 
 $('.menu-btn').click(function(){
-  $('#mobile-nav').toggleClass('active')
+  $('#mobile-nav').toggleClass("active")
+});
+
+window.addEventListener('mouseup', function(event) {
+  var menu = document.getElementById('#mobile-nav')
+  if (event.target != menu && event.target.parentNode != menu) {
+    $('#mobile-nav').removeClass("active");
+  }
 });
